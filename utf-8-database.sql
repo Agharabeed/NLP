8f28a649aab102c33fd0abffc3e73dfdfd1b8627
Agharabeed <47080352+Agharabeed@users.noreply.github.com>
2021-05-25 01:55:23 +0500

    Add files via upload

---
+++ utf-8-database.sql
@@ -0,0 +1,94 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.10-MariaDB : Database - sindhi
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(225) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+
+/*Data for the table `admin` */
+
+insert  into `admin`(`id`,`name`) values (1,'سنڌي ڊڪشنريون'),(2,'صوف');
+
+/*Table structure for table `api_translator` */
+
+DROP TABLE IF EXISTS `api_translator`;
+
+CREATE TABLE `api_translator` (
+  `id` int(11) NOT NULL,
+  `manages` tinyint(4) DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `api_translator` */
+
+/*Table structure for table `language` */
+
+DROP TABLE IF EXISTS `language`;
+
+CREATE TABLE `language` (
+  `id` int(11) NOT NULL,
+  `sindhi_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `language` */
+
+/*Table structure for table `language_sentence` */
+
+DROP TABLE IF EXISTS `language_sentence`;
+
+CREATE TABLE `language_sentence` (
+  `id` int(11) NOT NULL,
+  `language_id` int(11) DEFAULT NULL,
+  `sentence_segmentation` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `language_sentence` */
+
+/*Table structure for table `translator_repository` */
+
+DROP TABLE IF EXISTS `translator_repository`;
+
+CREATE TABLE `translator_repository` (
+  `id` int(11) NOT NULL,
+  `token` int(11) DEFAULT NULL,
+  `api_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `translator_repository` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(225) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
